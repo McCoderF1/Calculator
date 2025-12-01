d1b13e8f5b367aa060bdd0f1ff1b4bd7acbf9fef
John McLaren <john.mclaren.developer@gmail.com>
2025-12-01 20:01:31 +1300

    Added Add function

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Calculator.Engine
+{
+    public class Calculator
+    {
+        public int? Add(params int[] v)
+        {
+            int? result = null;
+
+            result = v[0];
+
+            if (v.Length > 1)
+            {
+                for (int i = 1; i < v.Length; i++)
+                {
+                    result += v[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_SingleValue_ValueReturned()
+        {
+            Calculator.Engine.Calculator calculator = new();
+
+            int? result = calculator.Add(5);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Add_TwoValues_ValuesAdded()
+        {
+            Calculator.Engine.Calculator calculator = new();
+
+            int? result = calculator.Add(5, 5);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+    }
+}
